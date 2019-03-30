d3df677a955a893a688912d6a1ab6b80fc1ed21f
Nina <ninarachaebuie@gmail.com>
2019-03-30 07:06:18 -0500

    Updates

---
+++ precache-manifest.2f1e59ba3c71b4720d4dc524753fa038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcc029df2a9495c0b5195af4d6bec64a",
+    "url": "/gnosis-pokedex/static/media/pkmnrs.bcc029df.ttf"
+  },
+  {
+    "revision": "299b4765edab689f1270",
+    "url": "/gnosis-pokedex/static/js/runtime~main.74c6e396.js"
+  },
+  {
+    "revision": "d322e432d3db92196727",
+    "url": "/gnosis-pokedex/static/js/main.d5a176da.chunk.js"
+  },
+  {
+    "revision": "fc8ee9df39e676c81489",
+    "url": "/gnosis-pokedex/static/js/2.88b3cb0d.chunk.js"
+  },
+  {
+    "revision": "d322e432d3db92196727",
+    "url": "/gnosis-pokedex/static/css/main.1bcca4a9.chunk.css"
+  },
+  {
+    "revision": "770176709a6ae92060664432daf00ecb",
+    "url": "/gnosis-pokedex/index.html"
+  }
+];
